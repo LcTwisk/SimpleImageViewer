c53b1684089579ea2d36e0d10d870245edb3d12b
Andreas Ganske <a.ganske@chaosspace.de>
2018-11-23 15:52:15 +0100

    Add scroll view setup closure

--- ImageViewer/ImageViewerConfiguration.swift
+++ ImageViewer/ImageViewerConfiguration.swift
@@ -3,11 +3,13 @@ import UIKit
 
 public typealias ImageCompletion = (UIImage?) -> Void
 public typealias ImageBlock = (@escaping ImageCompletion) -> Void
+public typealias ScrollViewBlock = (UIScrollView) -> Void
 
 public final class ImageViewerConfiguration {
     public var image: UIImage?
     public var imageView: UIImageView?
     public var imageBlock: ImageBlock?
+    public var setupScrollViewBlock: ScrollViewBlock?
     
     public typealias ConfigurationClosure = (ImageViewerConfiguration) -> ()
     
